e300bd9a4657a267d016228760a32fdf3815d048
Tomaz Cerar <tomaz.cerar@gmail.com>
2016-08-04 17:59:46 +0200

    WFLY-6901 - enable checkstyle for manualmode testsuite

--- testsuite/integration/manualmode/src/test/java/org/jboss/as/test/manualmode/jca/connectionlistener/TestConnectionListener.java
+++ testsuite/integration/manualmode/src/test/java/org/jboss/as/test/manualmode/jca/connectionlistener/TestConnectionListener.java
@@ -1,7 +1,6 @@
 package org.jboss.as.test.manualmode.jca.connectionlistener;
 
 
-
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -11,28 +10,27 @@ import org.jboss.logging.Logger;
 /**
  * @author <a href="mailto:hsvabek@redhat.com">Hynek Svabek</a>
  */
-public class TestConnectionListener implements ConnectionListener
-{
-   private static final Logger log = Logger.getLogger(TestConnectionListener.class);
-	
-   public TestConnectionListener(){
-   }
+public class TestConnectionListener implements ConnectionListener {
+    private static final Logger log = Logger.getLogger(TestConnectionListener.class);
 
-   public void initialize(ClassLoader cl) throws SQLException {
-   }
+    public TestConnectionListener() {
+    }
 
-   public void activated(Connection c) throws SQLException {
-	  c.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS test_table("
-			  +"description character varying(255),"
-			  +"type bigint"
-			+")");
-	  c.createStatement().executeUpdate("INSERT INTO test_table(description, type) VALUES ('activated', '1')");
-	  log.info("Activated record has been created, " + c);
-   }
+    public void initialize(ClassLoader cl) throws SQLException {
+    }
 
-   public void passivated(Connection c) throws SQLException {
-	   c.createStatement().executeUpdate("INSERT INTO test_table(description, type) VALUES ('passivated', '0')");
-	   log.info("Passivated record has been created, " + c);
-   }
+    public void activated(Connection c) throws SQLException {
+        c.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS test_table("
+                + "description character varying(255),"
+                + "type bigint"
+                + ")");
+        c.createStatement().executeUpdate("INSERT INTO test_table(description, type) VALUES ('activated', '1')");
+        log.info("Activated record has been created, " + c);
+    }
+
+    public void passivated(Connection c) throws SQLException {
+        c.createStatement().executeUpdate("INSERT INTO test_table(description, type) VALUES ('passivated', '0')");
+        log.info("Passivated record has been created, " + c);
+    }
 }
 
